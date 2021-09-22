8b2bb9dc7b4d77961bed10f5eede528e0cd78dda
dsafdasf <adsfasdfa>
2021-09-22 17:08:00 +0000

    test change

--- src/components/characterCount/characterCount.component.js
+++ src/components/characterCount/characterCount.component.js
@@ -3,6 +3,5 @@ import { charCountState } from '../../selector/charCount.selector';
 
 export default function CharacterCount() {
   const count = useRecoilValue(charCountState);
-
   return <div data-testid="test-character-count">Character Count: {count}</div>;
 }
