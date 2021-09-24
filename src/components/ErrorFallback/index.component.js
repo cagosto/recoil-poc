8edfca566799acd5ed5d6995d26fc8574d04ae70
dsafdasf <adsfasdfa>
2021-09-24 21:26:04 +0000

    fix test

--- src/components/ErrorFallback/index.component.js
+++ src/components/ErrorFallback/index.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <div role="alert">
+    <div role="alert" data-testid="test-error-message">
       <p>Something went wrong:</p>
       <pre>{error.message}</pre>
       <button onClick={resetErrorBoundary}>Try again</button>
--- src/components/Landing/Landing.error.test.js
+++ src/components/Landing/Landing.error.test.js
@@ -2,18 +2,21 @@ import { renderWithRouts, screen, waitFor } from '../../test/testing-library';
 import Landing from './landing';
 import { server } from '../../mocks/server';
 import { rest } from 'msw';
+import axios from 'axios';
+
+jest.mock('axios');
 
 /**
  * Error go in its own test file cause I cant figure out how to empty state after each test.
  */
 describe('App flow', () => {
   it('should display error', async () => {
-    server.resetHandlers(
-      rest.get(`${process.env.REACT_APP_DOMAIN}/users`, (req, res, ctx) =>
-        res(ctx.status(500))
-      )
-    );
-
+    // server.resetHandlers(
+    //   rest.get(`${process.env.REACT_APP_DOMAIN}/users`, (req, res, ctx) =>
+    //     res(ctx.status(500))
+    //   )
+    // );
+    axios.get.mockRejectedValueOnce();
     renderWithRouts(<Landing />);
     const landingPage = await screen.findByTestId('test-users-landing');
 
